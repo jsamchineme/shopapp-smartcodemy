1447e47b5da31003b70809ebaa49b5aa87887940
John Samuel <jsamchineme@gmail.com>
2019-09-21 12:29:11 +0100

    list products

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const setUpLocalStorage = () => {
+  const retrievedProducts = JSON.parse(localStorage.getItem('shopapp_products'));
+  if(retrievedProducts === null) {
+    localStorage.setItem('shopapp_products', "[]");
+  }
+}
+
+const products = JSON.parse(localStorage.getItem('shopapp_products'));
+const productsListContainer = document.querySelector('.product-list');
+
+function initialise() {
+  setUpLocalStorage();
+  renderProducts();
+}
+
+initialise();
+
+function renderProducts() {
+  let productListHTML = "";
+
+  products.forEach(product => {
+    const eachProductHTML = `
+      <div class="column item">
+        <div class="featured-image">
+          <img src="img/${product.image}" alt="">
+        </div>
+        <div class="product-description">
+          ${product.description}
+        </div>
+        <div class="details">
+          <span class='label'>Price: <span>${product.price}</span></span>
+          <span class="label">Quantity Available: <span>${product.quantity}</span></span>
+        </div>
+        <div class="actions">
+          <a class="button" href="product-view.html?id=${product.id}">View</a>
+          <button class="button">Edit</button>
+          <button class="button">Delete</button>
+        </div>
+      </div>
+    `;
+    productListHTML += eachProductHTML;
+  });
+  productsListContainer.innerHTML = productListHTML;
+}
+
